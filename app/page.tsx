74beff6d34b73530e7f156365a3010fa8df8d663
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-15 20:38:17 +0530

    added clerk authentication and dark theme 🚀

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <Button size="lg" variant="ghost">
-      Click me
-    </Button>
+    <div className="flex flex-col gap-y-4">
+      <h1>DashBoard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }
