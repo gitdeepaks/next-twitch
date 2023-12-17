16081caba9bceae2bafa2eee6c44546d7d158465
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-17 21:48:27 +0530

    added Navbar 🚀

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -2,8 +2,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
-      <h1>DashBoard</h1>
-      <UserButton afterSignOutUrl="/" />
+      <h1>HomePage</h1>
     </div>
   );
 }
