40900bba139d49cd058b9783534ca16021f30da6
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-18 20:53:50 +0530

    added sidebar 🚀

--- app/(browse)/_components/container.tsx
+++ app/(browse)/_components/container.tsx
@@ -2,13 +2,24 @@
 
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
+import React, { useEffect } from "react";
+import { useMediaQuery } from "usehooks-ts";
 
 interface ContainerProps {
   children: React.ReactNode;
 }
 
 export const Container = ({ children }: ContainerProps) => {
-  const { collapsed } = useSidebar((state) => state);
+  const { collapsed, onCollapse, onExpand } = useSidebar((state) => state);
+  const matches = useMediaQuery("(min-width: 1024px)");
+
+  useEffect(() => {
+    if (matches) {
+      onCollapse();
+    } else {
+      onExpand();
+    }
+  }, [matches, onCollapse, onExpand]);
 
   return (
     <div
