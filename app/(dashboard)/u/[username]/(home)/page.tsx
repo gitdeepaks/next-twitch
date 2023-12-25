eea24983c83f83ceb98b89bfe81de7681c6df4f0
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-25 09:58:40 +0530

    added viewer token 🚀

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,5 +1,26 @@
-const CreatorPage = () => {
-  return <div>CreatorPage</div>;
+import { StreamPlayer } from "@/components/stream-player";
+import { getUserByUserName } from "@/lib/user-service";
+import { currentUser } from "@clerk/nextjs";
+
+interface CreatorPageProps {
+  params: {
+    username: string;
+  };
+}
+
+const CreatorPage = async ({ params }: CreatorPageProps) => {
+  const externalUser = await currentUser();
+  const user = await getUserByUserName(params.username);
+
+  if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+    throw new Error("Unauthorized");
+  }
+
+  return (
+    <div className="h-full">
+      <StreamPlayer user={user} stream={user.stream} isFollowing={true} />
+    </div>
+  );
 };
 
 export default CreatorPage;
--- lib/user-service.ts
+++ lib/user-service.ts
@@ -3,6 +3,15 @@ import { db } from "./db";
 export const getUserByUserName = async (username: string) => {
   const user = await db.user.findUnique({
     where: { username },
+    include: { stream: true },
+  });
+  return user;
+};
+
+export const getUserById = async (id: string) => {
+  const user = await db.user.findUnique({
+    where: { id },
+    include: { stream: true },
   });
   return user;
 };
