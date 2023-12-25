67c24b056c64004bb851f325f6c80e2a1f5c7bc2
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-25 13:08:28 +0530

    added video component 🚀

---
+++ components/stream-player/fullscreen-control.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Maximize, Minimize } from "lucide-react";
+
+import { Hint } from "../hint";
+
+interface FullScreenControlProps {
+  isFullScreen: boolean;
+  onToggle: () => void;
+}
+
+export const FullScreenControl = ({
+  isFullScreen,
+  onToggle,
+}: FullScreenControlProps) => {
+  const Icon = isFullScreen ? Minimize : Maximize;
+
+  const label = isFullScreen ? "Exit full screen" : "Enter full screen";
+
+  return (
+    <div className="flex items-center justify-center gap-4">
+      <Hint label={label} asChild>
+        <button
+          onClick={onToggle}
+          className="text-white p-1.5 hover:bg-white/10 rounded-lg"
+        >
+          <Icon className="h-5 w-5" />
+        </button>
+      </Hint>
+    </div>
+  );
+};
