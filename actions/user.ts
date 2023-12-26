bc97e0705b39ee9849315aee15fb07377d642ee3
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-26 20:24:13 +0530

    added about card 🚀

---
+++ actions/user.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { getSelf } from "@/lib/auth-service";
+import { db } from "@/lib/db";
+import { User } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+export const updateUser = async (value: Partial<User>) => {
+  try {
+    const self = await getSelf();
+
+    const validData = {
+      bio: value.bio,
+    };
+
+    const user = await db.user.update({
+      where: { id: self.id },
+      data: { ...validData },
+    });
+    revalidatePath(`/${self.username}`);
+    revalidatePath(`/u/${self.username}`);
+
+    return user;
+  } catch (error) {
+    throw new Error("Intarnal Error");
+  }
+};
