98c4902ffd6001e7cb66ddb59bff3013e9a1be6c
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-27 11:08:19 +0530

    added home page 🚀

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,8 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
+import { Results, ResultsSkeleton } from "../_components/results";
+import { Suspense } from "react";
 export default function Home() {
   return (
     <div className="h-full p-8 max-w-screen-2xl mx-auto">
-      <h1>HomePage</h1>
+      <Suspense fallback={<ResultsSkeleton />}>
+        <Results />
+      </Suspense>
     </div>
   );
 }
