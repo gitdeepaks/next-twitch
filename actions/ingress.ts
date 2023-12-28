1b62386435c1f50655783f2f57bb2caba75aa53a
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-28 11:27:45 +0530

    migrating to next auth v5 with logout and bug fixing 🚀

--- actions/ingress.ts
+++ actions/ingress.ts
@@ -50,9 +50,9 @@ export const createIngress = async (ingressType: IngressInput) => {
   await resetIngressess(self.id);
 
   const options: CreateIngressOptions = {
-    name: self.username,
+    name: self.username!,
     roomName: self.id,
-    participantName: self.username,
+    participantName: self.username!,
     participantIdentity: self.id,
   };
 
